021294b2341f06a86a1993a55d7d6cfafccaa19b
mateotis <hekfuszmate@gmail.com>
2022-04-05 23:03:46 +0400

    Fixed wrong indexing on elevator buttons.

--- script.js
+++ script.js
@@ -102,8 +102,8 @@ document.body.addEventListener('keydown', (event) => {
 			elevatorMove.play();
 			$("#guide-3-1").fadeOut();
 			currentRoom = 3;
-			for (let i=0; i<10; i++) {
-				if (i === 7) {
+			for (let i=0; i<9; i++) {
+				if (i === 6) {
 					let buttonElem = document.getElementsByClassName("button")[i];
 					$(buttonElem).css('color', 'yellow');
 					// $(buttonElem).playKeyframe('flicker 5s linear 0s infinite', alternate);
@@ -119,7 +119,7 @@ document.body.addEventListener('keydown', (event) => {
 		}
 		else if(!isNaN(pressedKey) && pressedKey != 0) {
 			fakeButtonClick.play();
-			let buttonElement = document.getElementsByClassName("button")[pressedKey]; // The number on the button corresponds to its order in the HTML, so we can use it as a handy index
+			let buttonElement = document.getElementsByClassName("button")[pressedKey - 1]; // The number on the button corresponds to its order in the HTML, so we can use it as a handy index
 			$(buttonElement).animate({ opacity: 0 }); // Doing this instead of fadeOut() keeps the hidden element's "place" in the display
 		}
 	}
